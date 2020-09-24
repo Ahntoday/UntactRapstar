38b2156ee88b4d37726f470211c4f3a543deb99d
SeryoungShim <csssr1207@gmail.com>
2020-09-24 11:31:36 +0900

    add urls

--- rapProject/rapProject/urls.py
+++ rapProject/rapProject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rapApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', views.home, name="home"),
+    path('main/', views.choice, name="main"),
+    path('result/', views.result, name="result"),
+    path('save/', views.saveRecord, name="save"),
 ]
